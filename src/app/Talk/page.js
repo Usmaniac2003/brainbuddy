1e4b3c16e3cffc6464f30f1efa599c220de9a97d
Usman <usmancodepro@gmail.com>
2025-05-13 02:46:43 +0500

    v1.

--- src/app/Talk/page.js
+++ src/app/Talk/page.js
@@ -1,4 +1,4 @@
-import VoiceChat from "@/components/VoiceAgent/VoiceComponent";
+import VoiceChat from "../../components/VoiceAgent/VoiceComponent";
 
 export default function TalktoBrainBuddy(){
     return<>
